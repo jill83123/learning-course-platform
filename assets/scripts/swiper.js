39aaeb873c23857c64dac09cad2a2f7f0eeb2f70
YU-FANG <jill83123@gmail.com>
2024-08-25 04:11:52 +0800

    feat: 完成首頁切版

--- assets/scripts/swiper.js
+++ assets/scripts/swiper.js
@@ -16,3 +16,43 @@ const indexHeaderSwiper = new Swiper('#indexHeaderSwiper', {
   spaceBetween: 40,
   loop: true,
 });
+
+const courseSwiper = new Swiper('.course-swiper', {
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 'auto',
+});
+
+let cardMobileSwiper = null;
+function handleResize() {
+  const screenWidth = window.innerWidth;
+
+  if (!cardMobileSwiper && screenWidth < 992) {
+    cardMobileSwiper = new Swiper('.card-mobile-swiper', {
+      modules: [Navigation, Pagination],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      slidesPerView: 'auto',
+    });
+  }
+
+  if (cardMobileSwiper?.length > 0 && screenWidth > 992) {
+    cardMobileSwiper.forEach((swiper) => {
+      swiper.slideTo(0);
+      swiper.destroy(true, true);
+      cardMobileSwiper = null;
+    });
+  }
+}
+
+window.addEventListener('load', handleResize);
+window.addEventListener('resize', handleResize);
